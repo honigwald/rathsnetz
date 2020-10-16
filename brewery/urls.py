07e005d8de77a31f4696128cf1d94984da271165
honigwald <s.blanck@campus.tu-berlin.de>
2020-10-16 15:30:31 +0200

    refactoring

--- brewery/urls.py
+++ brewery/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('spindel/', views.spindel, name='spindel'),
     path('storage/', views.storage, name='storage'),
-    path('storage/new/', views.storage_add, name='storage_add'),
+    path('storage/add/', views.storage_add, name='storage_add'),
     path('storage/<int:s_id>/', views.storage_edit, name='storage_edit'),
     path('brewing/', views.brewing, name='brewing'),
-    path('brewing/new/', views.brewing_new, name='brewing_new'),
+    path('brewing/add/', views.brewing_add, name='brewing_add'),
     path('brewing/protocol/<int:cid>/', views.protocol, name='protocol'),
     path('recipe/', views.recipe, name='recipe'),
     path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('recipe/<int:recipe_id>/edit/', views.recipe_edit, name='recipe_edit'),
-    path('recipe/new/', views.recipe_new, name='recipe_new')
+    path('recipe/add/', views.recipe_add, name='recipe_add')
 ]
