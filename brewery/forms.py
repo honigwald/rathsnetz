064ba02a0a81b0e5f1334a0153cfa9ede8ee2c2d
honigwald <s.blanck@campus.tu-berlin.de>
2020-10-16 11:04:24 +0200

    finished first implementation of brewing process

--- brewery/forms.py
+++ brewery/forms.py
@@ -1,23 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-
-from .models import IngredientStorage, Recipe
-
 from .models import IngredientStorage, Recipe, Step
-
-class AddRecipe(forms.Form):
-    name = forms.CharField()
-
-
 
 
 class BrewingCharge(forms.Form):
     recipe = forms.ModelChoiceField(queryset=Recipe.objects.all().order_by('name'))
     amount = forms.FloatField()
     brewmaster = forms.ModelChoiceField(queryset=User.objects.all().order_by('username'))
+
 
 class BrewingProtocol(forms.Form):
     comment = forms.CharField(max_length=200)
@@ -36,12 +27,7 @@ class AddRecipe(ModelForm):
 
 
 class EditRecipe(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(EditRecipe,self).__init__(*args, **kwargs)
-        if 'recipe' in kwargs:
-            recipe = kwargs.pop('recipe')
-            self.fields['recipe'].initial = recipe
-
     class Meta:
         model = Step
         exclude = ('recipe',)
+
