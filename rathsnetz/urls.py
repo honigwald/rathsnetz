92e3bd0bb13fc5af4b57e6b0bf5ca0a746294694
honigwald <s.blanck@campus.tu-berlin.de>
2021-01-08 14:20:56 +0100

    working at frontend.

--- rathsnetz/urls.py
+++ rathsnetz/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('brewery.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
