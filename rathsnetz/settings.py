4584b28b8bb90b73362f50155f2facba65446cc2
honigwald <s.blanck@campus.tu-berlin.de>
2021-03-21 18:29:22 +0100

    timezone: europe/berlin

--- rathsnetz/settings.py
+++ rathsnetz/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -125,3 +125,7 @@ STATICFILES_DIRS = ('/home/simon/Documents/develop/develop/django/rathsnetz/stat
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+BOOTSTRAP4 = {
+    "include_jquery": False
+}
