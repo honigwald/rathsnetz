fc12bf2bb8446c252555502d2ee4b206bc8342cd
honigwald <s.blanck@campus.tu-berlin.de>
2021-03-24 20:15:01 +0100

    implemented timer

--- brewery/templatetags/custom_tags.py
+++ brewery/templatetags/custom_tags.py
@@ -5,11 +5,6 @@ register = template.Library()
 
 def seconds(td):
     total_seconds = int(td.total_seconds())
-    print("duration: filter")
-    print("{}".format(total_seconds))
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-
     return total_seconds
 
 
