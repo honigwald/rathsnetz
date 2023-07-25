36d53179497f6bc6e72e3f89f375479945616770
honigwald <sblanck@finitestate.de>
2023-07-25 08:38:44 +0200

    fixed: collect static did not work properly due to bad configuration

--- rathsnetz/settings.py
+++ rathsnetz/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
